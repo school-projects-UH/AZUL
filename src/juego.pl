eb0e2f95cb5b273c980c2b18c793ed2f54cc553b
Ginrod <gilbecubano@gmail.com>
2020-04-08 03:23:59 -0400

    master ok

--- src/juego.pl
+++ src/juego.pl
@@ -4,19 +4,30 @@
 
 % Los jugadores estan enumerados del 1 al 4
 
+% TEST CASE
+estado_muro(1, [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]).
+estado_muro(2, [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]).
+cant_jugadores(2).
+estado_puntuaciones(1, 0).
+estado_puntuaciones(2, 0).
+cant_rondas(1).
+estado_patrones(1, [[], [negro, 2], [azul, 1], [], []]).
+estado_patrones(2, [[azul, 1], [], [gris, 1], [], []]).
 
 % Predicados dinamicos
 :- dynamic
+   mejor_solucion/1,
+   posibles_jugadas/1,
    cant_fabricas/1,
    cant_jugadores/1,
    jugador_inicial/2,
-   estado_tapa_caja/2,
-   estado_bolsa/2,
-   estado_puntuaciones/3,
-   estado_fabricas/4,
-   estado_muro/3,
+   estado_tapa_caja/1,
+   estado_bolsa/1,
+   estado_puntuaciones/2,
+   estado_fabricas/1,
+   estado_muro/2,
    estado_suelo/4,
-   estado_patrones/4,
+   estado_patrones/2,
    cant_rondas/1,
    cant_turnos/2. % cant_turnos(Ronda, Turnos)
 
@@ -45,7 +56,7 @@ prepara_partida(Jugadores):-
     llena_bolsa(),
     inicializar_puntuaciones(Jugadores, N),
     inicializar_fabricas(CF, Fabricas),
-    asserta(estado_fabricas(0, 0, "", Fabricas)),
+    asserta(estado_fabricas(Fabricas)),
     inicializar_muros(Jugadores, N),
     inicializar_suelos(Jugadores, N),
     inicializar_patrones(Jugadores, N),
@@ -55,7 +66,7 @@ prepara_partida(Jugadores):-
 
     inicializar_puntuaciones([], 0).
     inicializar_puntuaciones([J|Rest_Jugadores], N):-
-        asserta(estado_puntuaciones(0, J, 0)),
+        asserta(estado_puntuaciones(J, 0)),
         M is N - 1,
         inicializar_puntuaciones(Rest_Jugadores, M).
 
@@ -67,7 +78,7 @@ prepara_partida(Jugadores):-
     inicializar_muros([], 0).
     inicializar_muros([J| Rest_jugadores], N):-
         M is N - 1,
-        asserta(estado_muro(0, J, [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])),
+        asserta(estado_muro(J, [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])),
         inicializar_muros(Rest_jugadores, M).
 
     inicializar_suelos([], 0).
@@ -79,7 +90,7 @@ prepara_partida(Jugadores):-
     inicializar_patrones([], 0).
     inicializar_patrones([J| Rest_jugadores], N):-
         M is N - 1,
-        asserta(estado_patrones(0, 0, J, [[""], ["", ""], ["", "", ""], ["", "", "", ""], ["", "", "", "", ""]])),
+        asserta(estado_patrones(J, [[], [], [], [], []])),
         inicializar_patrones(Rest_jugadores, M).
 
 
@@ -129,13 +140,13 @@ llena_fabricas(Bolsa_antes, N, Fabricas, Bolsa_despues):-
 
 % el primer jugador llena cada fabrica con 4 azulejos extraidos al azar
 
-mueve_azulejos_bolsa_fabrica(No_ronda, Jugador):-
-    estado_bolsa(No_ronda, Bolsa_antes),
+mueve_azulejos_bolsa_fabrica(No_ronda):-
+    estado_bolsa(Bolsa_antes),
     cant_fabricas(CF),
     llena_fabricas(Bolsa_antes, CF, Fabricas, Bolsa_despues),
-    retract(estado_bolsa(No_ronda, Bolsa_antes)),
-    asserta(estado_bolsa(No_ronda, Bolsa_despues)),
-    asserta(estado_fabricas(No_ronda, 1, Jugador, Fabricas)).
+    retract(estado_bolsa(Bolsa_antes)),
+    asserta(estado_bolsa(Bolsa_despues)),
+    asserta(estado_fabricas(Fabricas)).
 
 
 mueve_azulejos_fabrica_centro(Fabrica, Centro_antes, Centro_despues)
@@ -143,14 +154,13 @@ mueve_azulejos_fabrica_centro(Fabrica, Centro_antes, Centro_despues)
 
 
 mueve_azulejos_tapa_bolsa():-
-    cant_rondas(Ronda_actual),
-    estado_tapa_caja(Ronda_actual, Tapa),
-    retract(estado_tapa_caja(Ronda_actual, Tapa)),
-    asserta(estado_tapa_caja(Ronda_actual, [])),
-    estado_bolsa(Ronda_actual, Bolsa),
-    retract(estado_bolsa(Ronda_actual, Bolsa)),
+    estado_tapa_caja(Tapa),
+    retract(estado_tapa_caja(Tapa)),
+    asserta(estado_tapa_caja([])),
+    estado_bolsa(Bolsa),
+    retract(estado_bolsa(Bolsa)),
     append(Bolsa, Tapa, Nueva_bolsa),
-    asserta(estado_bolsa(Ronda_actual, Nueva_bolsa)).
+    asserta(estado_bolsa(Nueva_bolsa)).
 
 
 encuentra_fabrica(Fabricas, Color, F) :-
@@ -176,14 +186,14 @@ extrae_todos_azulejos_fabrica(Fabrica_antes, Azulejo_escogido, Fabrica_despues) 
 
 llena_bolsa():-
     llena_bolsa_(Bolsa),
-    asserta(estado_bolsa(0, Bolsa)).
+    asserta(estado_bolsa(Bolsa)).
 
     llena_bolsa_(Bolsa):-
-        llena_bolsa_color_([], "azul", 20, B1),
-        llena_bolsa_color_(B1, "rojo", 20, B2),
-        llena_bolsa_color_(B2, "amarillo", 20, B3),
-        llena_bolsa_color_(B3, "gris", 20, B4),
-        llena_bolsa_color_(B4, "negro", 20, Bolsa).
+        llena_bolsa_color_([], azul, 20, B1),
+        llena_bolsa_color_(B1, rojo, 20, B2),
+        llena_bolsa_color_(B2, amarillo, 20, B3),
+        llena_bolsa_color_(B3, gris, 20, B4),
+        llena_bolsa_color_(B4, negro, 20, Bolsa).
 
     llena_bolsa_color_(Bolsa_antes, _ , 0, Bolsa_antes).
     llena_bolsa_color_(Bolsa_antes, Color, Cantidad, Bolsa_despues):-
@@ -214,14 +224,14 @@ colores_iguales_en_muro(I0, J0, Muro, [V0, V1, V2, V3, V4]) :-
     valor_en(Muro, I3, J3, V3), I4 is I0+4, J4 is J0+4,
     valor_en(Muro, I4, J4, V4).
 
-actualiza_puntuacion_adicional(Jugador, Ronda, Puntuacion_adicional) :-
-    estado_puntuaciones(Ronda, Jugador, Puntuacion_actual),
-    Puntuacion_final is Puntuacion_actual + Puntuacion_adicional,
-    retract(estado_puntuaciones(Ronda, Jugador, Puntuacion_actual)),
-    asserta(estado_puntuaciones(Ronda, Jugador, Puntuacion_final)).
+actualiza_puntuacion_adicional(Jugador, Puntuacion_adicional) :-
+    estado_puntuaciones(Jugador, Puntuacion_actual),
+    Puntuacion_final is max(0, Puntuacion_actual + Puntuacion_adicional),
+    retract(estado_puntuaciones(Jugador, Puntuacion_actual)),
+    asserta(estado_puntuaciones(Jugador, Puntuacion_final)).
 
-puntua_adicional(Jugador, Ronda, Puntuacion_adicional) :-
-    estado_muro(Jugador, Ronda, Muro),
+puntua_adicional(Jugador, Puntuacion_adicional) :-
+    estado_muro(Jugador, Muro),
     contar_2pts_por_lineas_horizontales(Muro, Puntos_horizontales),
     contar_7pts_por_lineas_verticales(Muro, Puntos_verticales),
     contar_10pts_por_colores_completos(Muro, Puntos_colores),
@@ -263,32 +273,31 @@ puntua_adicional(Jugador, Ronda, Puntuacion_adicional) :-
 
 calcular_todos_los_puntos_adicionales() :-
     cant_jugadores(Ultimo_jugador),
-    cant_rondas(Ultima_ronda),
-    calcular_puntos_adicionales(Ultimo_jugador, Ultima_ronda).
+    calcular_puntos_adicionales(Ultimo_jugador).
 
-    calcular_puntos_adicionales(0, _) :- !.
-    calcular_puntos_adicionales(Jugador, Ultima_ronda) :-
-        puntua_adicional(Jugador, Ultima_ronda, Puntuacion_adicional),
-        actualiza_puntuacion_adicional(Jugador, Ultima_ronda, Puntuacion_adicional),
+    calcular_puntos_adicionales(0) :- !.
+    calcular_puntos_adicionales(Jugador) :-
+        puntua_adicional(Jugador, Puntuacion_adicional),
+        actualiza_puntuacion_adicional(Jugador, Puntuacion_adicional),
         Otro_jugador is Jugador - 1, !,
-        calcular_puntos_adicionales(Otro_jugador, Ultima_ronda).
+        calcular_puntos_adicionales(Otro_jugador).
 
 % Comprobar si la partida finalizó
-fin_partida(No_ronda, Termina) :-
+fin_partida(Termina) :-
     cant_jugadores(Ultimo_jugador),
-    comprobar_filas(Ultimo_jugador, No_ronda, 0, Termina).
+    comprobar_filas(Ultimo_jugador, 0, Termina).
 
-    comprobar_filas(0, _, _, 0) :- !.
-    comprobar_filas(Jugador, No_ronda, 0, Termina) :-
-        estado_muro(Jugador, No_ronda, Muro),
+    comprobar_filas(0, _, 0) :- !.
+    comprobar_filas(Jugador, 0, Termina) :-
+        estado_muro(Jugador, Muro),
         contar_2pts_por_lineas_horizontales(Muro, Puntos),
         Otro_jugador is Jugador - 1, !,
-        comprobar_filas(Otro_jugador, No_ronda, Puntos, Termina).
-    comprobar_filas(_, _, _, 1) :- !.
+        comprobar_filas(Otro_jugador, Puntos, Termina).
+    comprobar_filas(_, _, 1) :- !.
 
 % Puntuación por ronda
-puntua_jugador_ronda(Jugador, No_ronda, I, J, Puntuacion) :-
-    estado_muro(Jugador, No_ronda, Muro),
+puntua_jugador_ronda(Jugador, I, J, Puntuacion) :-
+    estado_muro(Jugador, Muro),
     I0 is I-1, I1 is I+1, J0 is J-1, J1 is J+1,
     adyacentes_izquierda(I, J0, Muro, 0, Cant_izq),
     adyacentes_derecha(I, J1, Muro, 0, Cant_der),
@@ -342,10 +351,10 @@ posicion_del_color_en_Muro(Color, Fila, Columna) :-
 
 % puede_poner_en_patron(No_patron, Patron, Muro, Azulejos, Exceso).
 puede_poner_en_patron(No_patron, [Color, Cant_patron], Muro, [Color, Cant_azulejos], Azulejos_sobrantes, Espacio_sobrante) :-
-    No_patron > Cant_patron, I is No_patron - 1, 
-    posicion_del_color_en_Muro(Color, I, J), 
+    No_patron > Cant_patron, I is No_patron - 1,
+    posicion_del_color_en_Muro(Color, I, J),
     valor_en(Muro, I, J, 0), !,
-    Espacio is No_patron - Cant_patron, 
+    Espacio is No_patron - Cant_patron,
     Azulejos_sobrantes is max(0, Cant_azulejos - Espacio),
     Espacio_sobrante is max(0, No_patron - Cant_patron - Cant_azulejos), !.
 
@@ -354,8 +363,8 @@ puede_poner_en_patron(No_patron, [], Muro, [Color, Cant], Azulejos_sobrantes, Es
     valor_en(Muro, I, J, 0), Azulejos_sobrantes is max(0, Cant - No_patron),
     Espacio_sobrante is max(0, No_patron - Cant), !.
 
-mueve_azulejos_a_linea_de_patron(_, [], [Color, Cant], [Color, Cant]) :- !.
-mueve_azulejos_a_linea_de_patron(No_patron, [Color, Cant_patron], [Color, Cant_azulejos], [Color, Cant_desp]) :-
+mueve_azulejos_a_linea_de_patron([], [Color, Cant], [Color, Cant]) :- !.
+mueve_azulejos_a_linea_de_patron([Color, Cant_patron], [Color, Cant_azulejos], [Color, Cant_desp]) :-
     Cant_desp is Cant_patron + Cant_azulejos.
 
 
@@ -402,43 +411,141 @@ nuevo_muro(Muro_viejo, I, J, Muro_nuevo) :-
     actualiza_posicion_del_muro(4, 3, Muro_viejo, I, J, Muro_nuevo), 
     actualiza_posicion_del_muro(4, 4, Muro_viejo, I, J, Muro_nuevo).
 
-actualiza_muro(Jugador, Ronda, I, J) :-
-    estado_muro(Jugador, Ronda, Muro_viejo),
+actualiza_muro(Jugador, I, J) :-
+    estado_muro(Jugador, Muro_viejo),
     nuevo_muro(Muro_viejo, I, J, Muro_nuevo),
-    retract(estado_muro(Jugador, Ronda, Muro_viejo)),
-    asserta(estado_muro(Jugador, Ronda, Muro_nuevo)),
-    puntua_jugador_ronda(Jugador, Ronda, I, J, P),
-    actualiza_puntuacion_adicional(Jugador, Ronda, P).
+    retract(estado_muro(Jugador, Muro_viejo)),
+    asserta(estado_muro(Jugador, Muro_nuevo)),
+    puntua_jugador_ronda(Jugador, I, J, P),
+    actualiza_puntuacion_adicional(Jugador, P).
 
-mover_lineas_de_patron_llenas(Jugador, Ronda) :-
-    cant_turnos(Ronda, Turno),
-    estado_patrones(Ronda, Turno, Jugador, [P1A, P2A, P3A, P4A, P5A]),
-    mover_azulejo_al_muro(Jugador, Ronda, 1, P1A, P1D), 
-    mover_azulejo_al_muro(Jugador, Ronda, 2, P2A, P2D), 
-    mover_azulejo_al_muro(Jugador, Ronda, 3, P3A, P3D), 
-    mover_azulejo_al_muro(Jugador, Ronda, 4, P4A, P4D), 
-    mover_azulejo_al_muro(Jugador, Ronda, 5, P5A, P5D),
-    actualizar_patrones(Ronda, Turno, Jugador, [P1A, P2A, P3A, P4A, P5A], [P1D, P2D, P3D, P4D, P5D]), !.
+mover_lineas_de_patron_llenas(Jugador) :-
+    estado_patrones(Jugador, [P1A, P2A, P3A, P4A, P5A]),
+    mover_azulejo_al_muro(Jugador, 1, P1A, P1D), 
+    mover_azulejo_al_muro(Jugador, 2, P2A, P2D), 
+    mover_azulejo_al_muro(Jugador, 3, P3A, P3D), 
+    mover_azulejo_al_muro(Jugador, 4, P4A, P4D), 
+    mover_azulejo_al_muro(Jugador, 5, P5A, P5D),
+    actualizar_patrones(Jugador, [P1A, P2A, P3A, P4A, P5A], [P1D, P2D, P3D, P4D, P5D]), !.
 
-actualizar_patrones(No_ronda, No_turno, Jugador, Patrones_antes, Patrones_desp) :- 
-    retract(estado_patrones(No_ronda, No_turno, Jugador, Patrones_antes)),
-    asserta(estado_patrones(No_ronda, No_turno, Jugador, Patrones_desp)).
+actualizar_patrones(Jugador, Patrones_antes, Patrones_desp) :- 
+    retract(estado_patrones(Jugador, Patrones_antes)),
+    asserta(estado_patrones(Jugador, Patrones_desp)).
 
-mover_azulejo_al_muro(Jugador, Ronda, N, [Color, N], []) :- 
+mover_azulejo_al_muro(Jugador, N, [Color, N], []) :- 
     I is N-1, posicion_del_color_en_Muro(Color, I, J),
-    actualiza_muro(Jugador, Ronda, I, J), !.
-mover_azulejo_al_muro(_, _, _, P, P).
+    actualiza_muro(Jugador, I, J), !.
+mover_azulejo_al_muro(_, _, P, P).
 
 alicatado_del_muro() :-
     cant_jugadores(Ultimo_jugador),
-    cant_rondas(Ronda_actual),
-    alicatar(Ultimo_jugador, Ronda_actual),
+    alicatar(Ultimo_jugador),
     retract(cant_rondas(Ronda_actual)),
     Siguiente_ronda is Ronda_actual + 1,
     asserta(cant_rondas(Siguiente_ronda)).
 
-    alicatar(0, _) :- !.
-    alicatar(Jugador, Ronda_actual) :-
-        mover_lineas_de_patron_llenas(Jugador, Ronda_actual),
+    alicatar(0) :- !.
+    alicatar(Jugador) :-
+        mover_lineas_de_patron_llenas(Jugador),
         Otro_jugador is Jugador - 1, !,
-        alicatar(Otro_jugador, Ronda_actual).
+        alicatar(Otro_jugador).
+
+
+% dada una fabrica, cuenta el numero de azulejos que hay de un color determinado
+
+numero_azulejos_fabrica(Fabrica, Color, Total):- numero_azulejos_fabrica(Fabrica, Color, 0, Total).
+numero_azulejos_fabrica([], _, Cantidad, Cantidad).
+numero_azulejos_fabrica( [Color|R], Color, Cantidad, Total):-
+    % print("Hello1"),
+    Nueva_cantidad is Cantidad + 1,
+    numero_azulejos_fabrica(R, Color, Nueva_cantidad, Total).
+numero_azulejos_fabrica( [_|R], Color, Cantidad, Total):-
+    % print("Hello2"),
+    numero_azulejos_fabrica(R, Color, Cantidad, Total).
+
+
+actualiza_solucion(F, C, P, AD):-
+    mejor_solucion(BF, BC, BP, BAD),
+    retract(mejor_solucion(BF, BC, BP, BAD)),
+    actualiza_solucion_(F, C, P, AD, BF, BC, BP, BAD).
+
+    actualiza_solucion_(F1, C1, P1, AD1, _, _, _, AD2):-
+        AD1 is min(AD1, AD2),
+        asserta(mejor_solucion(F1, C1, P1, AD1)).
+    actualiza_solucion_(_, _, _, AD1, F2, C2, P2, AD2):-
+        AD2 is min(AD1, AD2),
+        asserta(mejor_solucion(F2, C2, P2, AD2)).
+
+
+genera_todas_las_jugadas():-
+
+    cant_fabricas(CF),
+    asserta(posibles_jugadas([])),
+    a(CF).
+
+    a(0).
+    a(N):-
+        b(N, 5),
+        M is N - 1,
+        a(M).
+
+    b(_, 0).
+    b(F, N):-
+        c(F, N, 5),
+        M is N - 1,
+        b(F, M).
+
+    c(_, _, 0).
+    c(F, C, N):-
+        posibles_jugadas(L),
+        retract(posibles_jugadas(L)),
+        asserta(posibles_jugadas([[F, C, N]|L])),
+        M is N - 1,
+        c(F, C, M).
+
+color(1, rojo).
+color(2, azul).
+color(3, amarillo).
+color(4, gris).
+color(5, negro).
+
+ itera_por_todas_las_jugadas(Jugador):-
+    print("Entering: itera_por_todas_las_jugadas"), nl(),
+    posibles_jugadas(L), length(L, R), print(R), nl(),
+    asserta(mejor_solucion(1,1,1,1000)),
+    estado_patrones(Jugador, Patrones), print(Patrones), nl(),
+    estado_fabricas(Fabricas), print(Fabricas), nl(), nl(),
+    estado_muro(Jugador, Muro),  print(Muro), nl(),
+
+    itera(L, Fabricas, Patrones, Muro).
+
+    itera([], _, _, _).
+    itera([[F, C, P]|R], Fabricas, Patrones, Muro):-
+        print([F, C, P]), nl(),
+        nth1(F, Fabricas, Fabrica),
+        color(C, Color),
+        nth1(P, Patrones, Patron),
+        numero_azulejos_fabrica(Fabrica, Color, 0, Total), Total > 0, print(Patron), nl(),
+        puede_poner_en_patron(P, Patron, Muro, [Color, 1], _, _),
+        puede_poner_en_patron(P, Patron, Muro, [Color, Total], Azulejos_sobrantes1, Espacio_sobrante1),
+        print(Azulejos_sobrantes1), nl(), print(Espacio_sobrante1), nl(),
+        Dif is Azulejos_sobrantes1 - Espacio_sobrante1,
+        AD is abs(Dif), print("Dif"), print(Dif), nl(),
+        actualiza_solucion(F, C, P, AD),
+        itera(R, Fabricas, Patrones, Muro).
+
+    itera([[F, C, P]|R], Fabricas, Patrones, Muro):-
+         nth1(F, Fabricas, Fabrica), print(Fabrica), nl(),
+         color(C, Color), print(Color), nl(), print("IDColor:"), print(C), nl(),
+         nth1(P, Patrones, Patron), print(Patron), nl(),
+         numero_azulejos_fabrica(Fabrica, Color, 0, 0),
+         itera(R, Fabricas, Patrones, Muro).
+
+    itera([[F, C, P]|R], Fabricas, Patrones, Muro):-
+         print([F, C, P]), nl(),
+         nth1(F, Fabricas, Fabrica),
+         color(C, Color),
+         nth1(P, Patrones, Patron),
+         numero_azulejos_fabrica(Fabrica, Color, 0, Total), Total > 0, print(Patron), nl(),
+         not(puede_poner_en_patron(P, Patron, Muro, [Color, 1], _, _)),
+         itera(R, Fabricas, Patrones, Muro).
